9a24dfd50c29cad1c59d733ef44f667bc42735bc
sphixy225 <106871412+sphixy225@users.noreply.github.com>
2022-07-14 20:40:41 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import 'narrat/dist/lib.css';
+import "./css/main.css";
+import { startApp } from "narrat";
+
+window.addEventListener("load", () => {
+  startApp(
+    {
+      charactersPath: "data/characters.json",
+      configPath: "data/config.json",
+    },
+    {
+      debug: true,
+      logging: false,
+    }
+  );
+});
